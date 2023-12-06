3b8e4db15dfdd5c7bf0c5251bde8ab836ceef668
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2023-12-05 18:46:11 -0800

    Errors by error code added

---
+++ dev/errorCodes/_500_server.ts
@@ -0,0 +1,3 @@
+const _503_server_down = "Server currently down. Please come back another time",
+_500_server = "Server error Please give us a moment to fix this issue"
+export {_500_server, _503_server_down } 
---
+++ scripts/errorCodes/index.js
@@ -0,0 +1,3 @@
+import { _404, _404_product, _404_cat, _404_farm, _404_product_edit, _400_user, } from "./_400_user.js";
+import { _500_server, _503_server_down } from "./_500_server.js";
+export { _503_server_down, _400_user, _404, _404_product, _404_cat, _404_farm, _500_server, _404_product_edit, };
