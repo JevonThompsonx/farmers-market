f337b7c00713aaaf00f6cb07adf940b151dd26ca
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2023-12-11 23:21:05 -0800

    moved seed connection string

--- dev/seed/seeds.ts
+++ dev/seed/seeds.ts
@@ -1,12 +1,12 @@
-import mongoose from 'mongoose'; 
+import mongoose from "mongoose";
 //for accessing pw safely:
-import 'dotenv/config'; 
-const API_KEY = process.env.API_KEY; 
+import "dotenv/config";
+const API_KEY = process.env.API_KEY;
 
 //mongoose connection string
-import seedConnectionString from './seedConnectionString.js';
+import seedConnectionString from "./utils/seedConnectionString.js";
 
-import { groceryProduct } from '../models/products.js'
+import { groceryProduct } from "../models/products.js";
 
 // const testObject = new groceryProduct({
 //     name: 'gala apple',
@@ -18,47 +18,56 @@ import { groceryProduct } from '../models/products.js'
 
 // testObject.save().then(data=>console.log(data)).catch(err=>console.log(err))
 
-
-groceryProduct.insertMany([{
-    name: 'Pitted dates',
-    price:15.79 ,
-    size: 1, 
-    sizeType: 'lb',
-    qty: 20,
-    category: 'fruit'
-},{
-    name: 'Black seedless grapes',
-    price: 1.68,
-    size: 1, 
-    sizeType: 'lb',
-    qty: 100,
-    category: 'fruit'
-},{
-    name: 'Eggplant seed',
-    price: 7.96,
-    size:1 , 
-    sizeType: 'oz',
-    qty:10 ,
-    category: 'vegetable'
-},{
-    name: 'American cheese',
-    price:.311 ,
-    size: 1, 
-    sizeType: 'oz',
-    qty: 6000,
-    category: 'dairy'
-},{
-    name: 'Parmesan cheese',
-    price:.388 ,
-    size: 1, 
-    sizeType: 'oz',
-    qty: 5000,
-    category: 'dairy'
-},{
-    name: 'Oatmilk',
-    price:.109 ,
-    size: 1, 
-    sizeType: 'fl oz',
-    qty: 65000,
-    category: 'dairy'
-}]).then(data=>console.log(data)).catch(err=>console.log(err))
+groceryProduct
+	.insertMany([
+		{
+			name: "Pitted dates",
+			price: 15.79,
+			size: 1,
+			sizeType: "lb",
+			qty: 20,
+			category: "fruit",
+		},
+		{
+			name: "Black seedless grapes",
+			price: 1.68,
+			size: 1,
+			sizeType: "lb",
+			qty: 100,
+			category: "fruit",
+		},
+		{
+			name: "Eggplant seed",
+			price: 7.96,
+			size: 1,
+			sizeType: "oz",
+			qty: 10,
+			category: "vegetable",
+		},
+		{
+			name: "American cheese",
+			price: 0.311,
+			size: 1,
+			sizeType: "oz",
+			qty: 6000,
+			category: "dairy",
+		},
+		{
+			name: "Parmesan cheese",
+			price: 0.388,
+			size: 1,
+			sizeType: "oz",
+			qty: 5000,
+			category: "dairy",
+		},
+		{
+			name: "Oatmilk",
+			price: 0.109,
+			size: 1,
+			sizeType: "fl oz",
+			qty: 65000,
+			category: "dairy",
+		},
+	])
+	.then((data) => console.log(data))
+	.catch((err) => console.log(err));
