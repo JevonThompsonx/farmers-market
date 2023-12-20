8c72787177a5cc2bf52260724d47bb8cf88fe350
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2023-12-20 14:02:38 -0800

    Farm Schema update

--- dev/models/farms.ts
+++ dev/models/farms.ts
@@ -1,21 +1,35 @@
 import mongoose, { Schema, model } from "mongoose";
 import getBing from "../seed/utils/getBing.js";
 import Joi from "joi";
-const ObjectId = Schema.Types.ObjectId 
-, farmSchema = new Schema({
+const ObjectId = Schema.Types.ObjectId,
+	farmLocationSchema = new Schema({
+		city: {
+			type: String,
+		},
+		state: {
+			type: String,
+		},
+	}),
+	farmSchema = new Schema({
 		name: {
 			type: String,
 			required: [true, "Cannot create a nameless farm"],
 		},
-		city: {
-			type: String,
-		},
+		location: farmLocationSchema,
 		description: {
 			type: String,
+			default: " ",
+			required: false,
 		},
 		email: {
 			type: String,
-			required: [true, "Cannot create a farm w/o an email"],
+			required: [
+				true,
+				"Cannot create a farm w/o an email. Please complete account set up",
+			],
+		},
+		imageLink: {
+			type: String,
 		},
 		products: [
 			{
@@ -23,6 +37,15 @@ const ObjectId = Schema.Types.ObjectId
 				ref: "products",
 			},
 		],
+		created: {
+			type: Date,
+			default: new Date(),
+			required: false,
+		},
+		updated: {
+			type: Date,
+			required: false,
+		},
 	}),
 	joiFarmSchema = Joi.object({
 		name: Joi.string().required(),
@@ -30,5 +53,16 @@ const ObjectId = Schema.Types.ObjectId
 		city: Joi.string(),
 		email: Joi.string().required(),
 	});
+
+farmSchema.pre("save", async function (next) {
+	this.updated = new Date();
+	const newImageLink = await getBing("Farm");
+	if (!this.imageLink || this.imageLink != newImageLink) {
+		this.imageLink = newImageLink;
+	}
+
+	next();
+});
+
 const farm = model("farm", farmSchema);
 export { farmSchema, farm, joiFarmSchema };
--- scripts/models/farms.js
+++ scripts/models/farms.js
@@ -1,19 +1,33 @@
 import { Schema, model } from "mongoose";
+import getBing from "../seed/utils/getBing.js";
 import Joi from "joi";
-const ObjectId = Schema.Types.ObjectId, farmSchema = new Schema({
+const ObjectId = Schema.Types.ObjectId, farmLocationSchema = new Schema({
+    city: {
+        type: String,
+    },
+    state: {
+        type: String,
+    },
+}), farmSchema = new Schema({
     name: {
         type: String,
         required: [true, "Cannot create a nameless farm"],
     },
-    city: {
-        type: String,
-    },
+    location: farmLocationSchema,
     description: {
         type: String,
+        default: " ",
+        required: false,
     },
     email: {
         type: String,
-        required: [true, "Cannot create a farm w/o an email"],
+        required: [
+            true,
+            "Cannot create a farm w/o an email. Please complete account set up",
+        ],
+    },
+    imageLink: {
+        type: String,
     },
     products: [
         {
@@ -21,11 +35,28 @@ const ObjectId = Schema.Types.ObjectId, farmSchema = new Schema({
             ref: "products",
         },
     ],
+    created: {
+        type: Date,
+        default: new Date(),
+        required: false,
+    },
+    updated: {
+        type: Date,
+        required: false,
+    },
 }), joiFarmSchema = Joi.object({
     name: Joi.string().required(),
     description: Joi.string(),
     city: Joi.string(),
     email: Joi.string().required(),
+});
+farmSchema.pre("save", async function (next) {
+    this.updated = new Date();
+    const newImageLink = await getBing("Farm");
+    if (!this.imageLink || this.imageLink != newImageLink) {
+        this.imageLink = newImageLink;
+    }
+    next();
 });
 const farm = model("farm", farmSchema);
 export { farmSchema, farm, joiFarmSchema };
