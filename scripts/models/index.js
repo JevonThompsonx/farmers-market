c191ad452f6c835390f07b467338f7de73992fb7
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2023-12-20 16:24:47 -0800

    removed unused exports

--- scripts/models/index.js
+++ scripts/models/index.js
@@ -1,5 +1,4 @@
 import { farm, farmSchema, joiFarmSchema } from "./farms.js";
 import { groceryProduct, groceryProductSchema, joiProductSchema, } from "./products.js";
-import { farmImageSchema, joiFarmImageSchema, farmImage, } from "./farmImages.js";
 import { usLocation, usLocationSchema } from "./states.js";
-export { farm, farmSchema, groceryProduct, groceryProductSchema, joiFarmSchema, joiProductSchema, usLocation, usLocationSchema, farmImageSchema, joiFarmImageSchema, farmImage, };
+export { farm, farmSchema, groceryProduct, groceryProductSchema, joiFarmSchema, joiProductSchema, usLocation, usLocationSchema, };
