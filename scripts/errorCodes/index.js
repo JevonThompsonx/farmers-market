474f02b92926a55c4a0f672a4f261fbaa328af8c
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-01-18 22:42:16 -0800

    Updated error codes

--- scripts/errorCodes/index.js
+++ scripts/errorCodes/index.js
@@ -1,3 +1,3 @@
-import { _404, _404_product, _404_cat, _404_farm, _404_product_edit, _400_user, } from "./_400_user.js";
+import { _404, _404_product, _404_cat, _404_farm, _404_edit, _400_user, } from "./_400_user.js";
 import { _500_server, _503_server_down } from "./_500_server.js";
-export { _503_server_down, _400_user, _404, _404_product, _404_cat, _404_farm, _500_server, _404_product_edit, };
+export { _503_server_down, _400_user, _404, _404_product, _404_cat, _404_farm, _500_server, _404_edit, };
