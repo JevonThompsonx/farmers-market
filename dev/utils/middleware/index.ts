68870b4cdc6e56bf73ae90b743b07f9889874982
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-02-19 15:18:14 -0800

    Update index.ts

--- dev/utils/middleware/index.ts
+++ dev/utils/middleware/index.ts
@@ -1,4 +1,5 @@
 import {joiFarmCreationValiation,joiFarmEditValiation} from "./joiFarmValidate.js";
 import { joiProductCreationValidation, joiProductEditValidation} from "./joiProductValidate.js";
+import { joiReviewValidate } from "./joiReviewValidate.js";
+export { joiFarmCreationValiation,joiFarmEditValiation,joiReviewValidate, joiProductCreationValidation,joiProductEditValidation };
 
-export { joiFarmCreationValiation,joiFarmEditValiation, joiProductCreationValidation,joiProductEditValidation };
