90a1625ebecd60fdd8ed1a2e1967a50e02216846
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-02-26 23:25:26 -0800

    Fixed product review not working

--- scripts/models/index.js
+++ scripts/models/index.js
@@ -2,4 +2,4 @@ import { farm, farmSchema, joiFarmSchema } from "./farms.js";
 import { groceryProduct, groceryProductSchema, joiProductSchema, joiProductEditSchema, } from "./products.js";
 import { usLocation, usLocationSchema } from "./states.js";
 import { review, reviewSchema, joiReviewSchema } from "./reviews.js";
-export { farm, farmSchema, groceryProduct, groceryProductSchema, joiFarmSchema, joiProductSchema, joiProductEditSchema, usLocation, usLocationSchema, review, reviewSchema, joiReviewSchema };
+export { farm, farmSchema, groceryProduct, groceryProductSchema, joiFarmSchema, joiProductSchema, joiProductEditSchema, usLocation, usLocationSchema, review, reviewSchema, joiReviewSchema, };
