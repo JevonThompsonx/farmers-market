4e398759d7605b6466e726958adf7e76ccc65eff
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-03-07 10:34:17 -0800

    Created random review generator

---
+++ dev/seed/utils/generateReview.ts
@@ -0,0 +1,18 @@
+import { review } from "../../models/index.js";
+import { randomIntGen } from "../../utils/index.js";
+import { Schema } from "mongoose";
+
+const reviewArray = ["⭐", "⭐⭐", "⭐⭐⭐", "⭐⭐⭐⭐", "⭐⭐⭐⭐⭐"],
+  randReview = () => {
+    const randRatingFunc = () => randomIntGen(5),
+      randRating = randRatingFunc(),
+      randReview = new review({
+        body: "Testing review",
+        rating: randRating,
+        ratingInStars: reviewArray[randRating],
+      });
+    randReview.save();
+    return randReview._id;
+  };
+
+export { randReview };
